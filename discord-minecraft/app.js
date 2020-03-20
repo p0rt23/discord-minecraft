b1a0df13e5d1ccc996429b819e25a3f5a0026402
Mark Smith <msmith4@gmail.com>
2020-03-20 03:01:42 +0000

    Added elasticsearch

--- discord-minecraft/app.js
+++ discord-minecraft/app.js
@@ -1,11 +1,31 @@
+'use strict'
+
 require('dotenv').config();
-const Discord = require('discord.js');
-
 const token  = process.env.TOKEN;
-const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.info(`minecraft-bot: Logged in as ${client.user.tag}!`);
+const { Client } = require('@elastic/elasticsearch')
+const elastic = new Client({ node: 'http://elasticsearch:9200' })
+
+const Discord = require('discord.js');
+const discord = new Discord.Client();
+
+discord.on('ready', () => {
+    console.info(`minecraft-bot: Logged in as ${discord.user.tag}!`);
 });
 
-client.login(token);
+discord.login(token);
+
+async function run() {
+    const { body } = await elastic.search({
+        index: 'filebeat-*',
+        body: {
+            query: {
+                match: { message: 'client' }
+            }
+        }
+    })
+        
+    console.log(body.hits.hits)
+}
+
+run().catch(console.log)
