20fad659daeac777103fbca8e761418346562349
Mark Smith <msmith4@gmail.com>
2020-05-07 03:01:24 +0000

    Working on Elastic query

--- discord-minecraft/app.js
+++ discord-minecraft/app.js
@@ -1,10 +1,10 @@
-'use strict'
+'use strict';
 
 require('dotenv').config();
 const token  = process.env.TOKEN;
 
-const { Client } = require('@elastic/elasticsearch')
-const elastic = new Client({ node: 'http://elasticsearch:9200' })
+const { Client } = require('@elastic/elasticsearch');
+const elastic = new Client({ node: 'http://elasticsearch:9200' });
 
 const Discord = require('discord.js');
 const discord = new Discord.Client();
@@ -33,10 +33,38 @@ async function get_logins(hours) {
             index: 'filebeat-*',
             body: {
                 query: {
-                    match: { message: 'telemetry' }
+                    "bool": {
+                        "must": [],
+                        "filter": [
+                            {
+                                "bool": {
+                                    "should": [{
+                                        "match": {
+                                            "message": "restarting"
+                                        }
+                                    }],
+                                    "minimum_should_match": 1
+                                }
+                            },
+                            {
+                                "match_phrase": {
+                                    "container.name": "filebeat"
+                                }
+                            },
+                            {
+                                "range": {
+                                    "@timestamp": {
+                                        "gte": "2020-05-06T06:00:00.000Z",
+                                        "lte": "2020-05-07T05:59:59.999Z",
+                                        "format": "strict_date_optional_time"
+                                    }
+                                }
+                            }
+                        ],
+                    }
                 }
             }
-        })
+        });
         return `Logins for past ${hours} hours: ${body.hits.total.value}`;
     }
     catch(e) {
