71f46d337e56ae56af9ec633de6551f1d1b73d57
Mark Smith <msmith4@gmail.com>
2020-05-08 23:21:18 +0000

    Bunyan and eslint

--- discord-minecraft/app.js
+++ discord-minecraft/app.js
@@ -1,73 +1,76 @@
-'use strict';
+'use strict'
 
-require('dotenv').config();
-const token  = process.env.TOKEN;
+const bunyan = require('bunyan')
+const log = bunyan.createLogger({ name: 'discord-minecraft' })
 
-const { Client } = require('@elastic/elasticsearch');
-const elastic = new Client({ node: 'http://elasticsearch:9200' });
+require('dotenv').config()
+const token = process.env.TOKEN
 
-const Discord = require('discord.js');
-const discord = new Discord.Client();
+const { Client } = require('@elastic/elasticsearch')
+const elastic = new Client({ node: 'http://elasticsearch:9200' })
+
+const Discord = require('discord.js')
+const discord = new Discord.Client()
 
 discord.on('ready', () => {
-    console.info(`minecraft-bot: Logged in as ${discord.user.tag}!`);
-});
+  log.info(`minecraft-bot: Logged in as ${discord.user.tag}!`)
+})
 
-discord.login(token);
+discord.login(token)
 
 discord.on('message', msg => {
-    if (msg.content === '!mc-logins') {
-        console.info(`minecraft-bot: ${msg.author.username} said "${msg.content}"`);
-        
-        get_logins(24).then(logins => {
-            if (logins) {
-                msg.reply(logins);
-            }
-        });
-    }
-});
+  if (msg.content === '!mc-logins') {
+    log.info(`minecraft-bot: ${msg.author.username} said "${msg.content}"`)
 
-async function get_logins(hours) {
-    try {
-        const { body } = await elastic.search({
-            index: 'filebeat-*',
-            body: {
-                query: {
-                    "bool": {
-                        "must": [],
-                        "filter": [
-                            {
-                                "bool": {
-                                    "should": [{
-                                        "match": {
-                                            "message": "restarting"
-                                        }
-                                    }],
-                                    "minimum_should_match": 1
-                                }
-                            },
-                            {
-                                "match_phrase": {
-                                    "container.name": "filebeat"
-                                }
-                            },
-                            {
-                                "range": {
-                                    "@timestamp": {
-                                        "gte": "2020-05-06T06:00:00.000Z",
-                                        "lte": "2020-05-07T05:59:59.999Z",
-                                        "format": "strict_date_optional_time"
-                                    }
-                                }
-                            }
-                        ],
+    getLogins(24).then(logins => {
+      if (logins) {
+        msg.reply(logins)
+      } else {
+        msg.reply('No logins!')
+      }
+    })
+  }
+})
+
+async function getLogins (hours) {
+  try {
+    const { body } = await elastic.search({
+      index: 'filebeat-*',
+      body: {
+        query: {
+          bool: {
+            must: [],
+            filter: [
+              {
+                bool: {
+                  should: [{
+                    'match-phrase': {
+                      message: 'joined the game'
                     }
+                  }],
+                  minimum_should_match: 1
                 }
-            }
-        });
-        return `Logins for past ${hours} hours: ${body.hits.total.value}`;
-    }
-    catch(e) {
-        console.error(e);
-    }
+              },
+              {
+                match_phrase: {
+                  'container.name': 'minecraft'
+                }
+              },
+              {
+                range: {
+                  '@timestamp': {
+                    gte: 'now-1d/d',
+                    lte: 'now/d'
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    })
+    return `Logins for past ${hours} hours: ${body.hits.total.value}`
+  } catch (e) {
+    log.error(e)
+  }
 }
