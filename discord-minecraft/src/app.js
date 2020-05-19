d72023a9a0c1dfc32fd131b8700e8f169adfa6a3
Mark Smith <msmith4@gmail.com>
2020-05-19 00:01:43 +0000

    Bugfix: Fixed logging

--- discord-minecraft/src/app.js
+++ discord-minecraft/src/app.js
@@ -17,17 +17,23 @@ class DiscordMinecraft {
       name: loggerName,
       level: 'debug'
     })
-    this.logfile = new Tail('/minecraft/logs/latest.log')
-    this.elastic = new Elastic(elasticUrl, this.log)
-    this.discord = new Discord(token, this.log)
+    try {
+      this.logfile = new Tail('/minecraft/logs/latest.log', { force: true })
+      this.elastic = new Elastic(elasticUrl, this.log)
+      this.discord = new Discord(token, this.log)
+    } catch (e) {
+      this.log.error(e)
+    }
   }
 
   run () {
     this.registerEvents()
+    this.logfile.start()
   }
 
   registerEvents () {
     this.logfile.on('line', line => this.handleOnLine(line))
+    this.logfile.on('error', err => this.log.error(err))
     this.discord.client.on('ready', () => { this.handleOnReady() })
     this.discord.client.on('message', msg => { this.handleOnMessage(msg) })
   }
